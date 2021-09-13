0fa48a88eed07c8ec6b2ca39ac6b0a594c516b32
osara22 <osaraiya22@gmail.com>
2021-09-13 11:32:01 +0000

    [Add]youtube埋め込み

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,4 +3,10 @@ class Blog < ApplicationRecord
   attachment :body_image
 
   validates :title, :body, presence: true
+
+  # youtubeのURLのIDを抽出する
+  def split_id_from_youtube_url
+    url = video_url.gsub("=","/")
+    url.split('/').last
+  end
 end
