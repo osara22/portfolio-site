2bfc90caae697ce40282b0eed8655eb474985312
osara22 <osaraiya22@gmail.com>
2021-10-15 11:12:42 +0000

    ログイン後のリダイレクト先変更とsystem_supportのテスト作成

--- spec/support/system_support.rb
+++ spec/support/system_support.rb
@@ -1,10 +1,15 @@
 module SystemSupport
   def login_admin(admin)
     visit new_admin_session_path
-    fill_in 'Email', with: admin.email
-    fill_in 'Password', with: admin.password
+    fill_in 'admin[email]', with: admin.email
+    fill_in 'admin[password]', with: admin.password
     click_on 'ログイン'
   end
 
-
+  def login_user(user)
+    visit new_user_session_path
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    click_on 'ログイン'
+  end
 end
--- spec/system/system_support_test.rb
+++ spec/system/system_support_test.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-describe 'system_supportがちゃんと動くかテスト', type: :system do
+describe 'system_supportの動作確認', type: :system do
 
-  describe 'registrations' do
+  describe 'user新規登録の動作確認' do
     before do
       visit new_user_registration_path
     end
@@ -23,7 +23,7 @@ describe 'system_supportがちゃんと動くかテスト', type: :system do
       it 'password確認フォームが表示される' do
         expect(page).to have_field 'user_password_confirmation'
       end
-      it 'Log inボタンが表示される' do
+      it '新規登録ボタンが表示される' do
         expect(page).to have_button '新規登録'
       end
     end
@@ -31,15 +31,82 @@ describe 'system_supportがちゃんと動くかテスト', type: :system do
    before do
      visit new_user_registration_path
    end
-    context 'sign_up成功のテスト' do
-      it 'sign_up後のリダイレクト先が、sign_upしたユーザの詳細画面になっている' do
+    context 'sign_up成功の確認' do
+      it 'sign_up後のリダイレクト先が、user詳細ページになっている' do
         fill_in 'user[email]', with:  'abcd@example.com'
         fill_in 'user[name]', with: 'namaee'
         fill_in 'user[password]', with: 'password'
         fill_in 'user[password_confirmation]', with: 'password'
         click_button '新規登録'
 
-        expect(page).to have_content '新着記事'
+        expect(page).to have_content 'ユーザーページ'
+        expect(current_path).to eq '/users/' + User.last.id.to_s
+      end
+    end
+  end
+
+  describe 'userログインページの動作確認' do
+    before do
+      visit new_user_session_path
+    end
+
+    context '表示内容の確認' do
+      it 'URLが正しい' do
+        expect(current_path).to eq '/users/sign_in'
+      end
+      it 'emailフォームが表示される' do
+        expect(page).to have_field 'user_email'
+      end
+      it 'passwordフォームが表示される' do
+        expect(page).to have_field 'user_password'
+      end
+      it 'ログインボタンが表示される' do
+        expect(page).to have_button 'ログイン'
+      end
+    end
+
+    context 'ログインの動作確認(login_userメソッド)' do
+      let(:user) { create(:user) }
+
+      it 'sign_in後のリダイレクト先が、ユーザー詳細ページになっている' do
+        visit new_user_session_path
+        fill_in 'user[email]', with: user.email
+        fill_in 'user[password]', with: user.password
+        click_on 'ログイン'
+        expect(current_path).to eq '/users/' + User.last.id.to_s
+      end
+    end
+  end
+
+  describe 'adminログインページの動作確認' do
+    before do
+      visit new_admin_session_path
+    end
+
+    context '表示内容の確認' do
+      it 'URLが正しい' do
+        expect(current_path).to eq '/admins/sign_in'
+      end
+      it 'emailフォームが表示される' do
+        expect(page).to have_field 'admin_email'
+      end
+      it 'passwordフォームが表示される' do
+        expect(page).to have_field 'admin_password'
+      end
+      it 'ログインボタンが表示される' do
+        expect(page).to have_button 'ログイン'
+      end
+    end
+
+    context 'ログインの動作確認(login_adminメソッド)' do
+      let(:admin) { create(:admin) }
+
+      it 'sign_in後のリダイレクト先が、adminのトップページになっている' do
+        visit new_admin_session_path
+        fill_in 'admin[email]', with: admin.email
+        fill_in 'admin[password]', with: admin.password
+        click_on 'ログイン'
+        expect(current_path).to eq '/admin'
       end
     end
   end
