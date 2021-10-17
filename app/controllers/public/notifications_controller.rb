d1121de85e097ce6decce2891c110943d7858c42
osara22 <osaraiya22@gmail.com>
2021-10-17 15:02:20 +0000

    通知機能追加途中

---
+++ app/controllers/public/notifications_controller.rb
@@ -0,0 +1,9 @@
+class Public::NotificationsController < ApplicationController
+  before_action :user_not_signin
+  def index
+    @notifications = current_user.passive_notifications.page(params[:page]).per(20)
+    @notifications.where(checked: false).each do |notification|
+      notification.update_attributes(checked: true)
+    end
+  end
+end
